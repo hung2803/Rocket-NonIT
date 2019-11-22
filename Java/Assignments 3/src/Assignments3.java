dfa89b7794bf46f1a84991cba0df0d11d33b6925
hung2803 <hungnt283@gmail.com>
2019-11-22 14:43:30 +0700

    bai tap java1

---
+++ Java/Assignments 3/src/Assignments3.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+
+public class Assignments3 {
+	public static void main(String[] args) {
+		// Tạo điểm cho sinh viên theo 5 môn sau:
+		// phân tích yêu cầu, thiết kế phần mềm, lập trình Java, Code và Unit
+		// Test, Management Tool.
+		// Mỗi môn có chứa:
+		// o 1 sinh viên có điểm từ 8 --> 10
+		// o 3 sinh viên có điểm từ 5 --> 7
+		// o 3 sinh viên có điểm từ 3 --> 4
+		// o 2 sinh viên có điểm 1 hoặc 2
+		// o 1 sinh viên có điểm 0.
+
+		Random random = new Random();
+		String[] diem = new String[10];
+		int diem_Number;
+		for (int i = 0; i < 10; i++) {
+			diem_Number = random.nextInt(9999999 - 1000000 + 1) + 1000000;
+		
+		
+		
+	}
+}}
